9ccbd4f54871242d633c8133f9889ea0cbd14ccc
Alireza Saberi <saberi.ar.eng@gmail.com>
2016-08-20 11:34:00 -0400

    feat: chekcing for video support
    checking the availability of video of html5 in browser by creating the
    element and checking if it has canPlayType property in it.
    
    [ticket : X]

--- checker.js
+++ checker.js
@@ -2,7 +2,7 @@
 * @Author: Ali
 * @Date:   2016-08-20 05:26:39
 * @Last Modified by:   Ali
-* @Last Modified time: 2016-08-20 07:00:07
+* @Last Modified time: 2016-08-20 11:31:53
 */
 /*This JS file is based on up and running HTML5 book*/
 
@@ -12,7 +12,8 @@ function html5checker_init(){
     console.log( "checking different HTML5 features ..." );
     var html5checker = {
         canvas : false,
-        canvastext: false
+        canvastext: false,
+        canplayvideo: false
     }
     console.log( "Canvas support: " html5checker.canvas );
     console.log( "Canvas text support: " html5checker.canvastext );
@@ -20,13 +21,16 @@ function html5checker_init(){
 
 function supports_canvas(){
     /*  check the support of the canvas by creating canvas element and checking
-    if there is getContext method in it*/
+    if there is getContext method in it
+    checking method : creating element, checking if certain property exist for it or not*/
     html5checker.canvas == !! document.createElement( 'canvas' ).getContext;
 }
 
 function supports_canvas_text() {
     /* canvas text api was added later, so some browsers shipped with canvas support
-    before text api was complete*/
+    before text api was complete.
+    checking method : creating element, checking if certain property exist for it or not.
+    Here, it's fillText*/
     if ( !html5checker.canvas ){
     } else {
         var context = document.createElement( 'canvas' ).getContext('2d');
@@ -35,4 +39,15 @@ function supports_canvas_text() {
         }
         else {}
     }
+}
+
+function support_videoplay(){
+    /*check if the browser is capable of playing video
+    checking method : creating element, checking if certain property exist for it or not.
+    Here, it's canPlayType */
+    if ( !!document.createElement('video').cabPlayType ) {
+        html5checker.canplayvideo == true;
+    }
+    else {}
+
 }
